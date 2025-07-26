029fe3f866dbf7dffce73d3015168c8e2f2a3393
bluemau531 <plazacabello531@gmail.com>
2025-07-26 12:59:46 -0500

    Logic and ability to color

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
 // Reference to Div in HTML Doc
 const container = document.getElementById("container");
+// Style for Container
+container.style.display = "inline-block";
+container.style.width = "auto";
+container.style.border = "5px solid white";
 
-// Grid Loop 16x16
-for(let t=0; t<16; t++){
 
+// Logic for Holding Down Mouse
+let mouseDown = false;
+
+document.addEventListener("mousedown", () => mouseDown = true);
+document.addEventListener("mouseup", () => mouseDown = false);
+
+grid();
+
+function grid(){
+    // Grid Loop 16x16
+    for(let t=0; t<16; t++){
     // Rows 1-16
     const row = document.createElement("div");
     row.style.display = "flex";
+        for(let r=0; r<16; r++){
+            // The Div Element Box
+            const div = document.createElement("div");
+            // Styling the Div Boxes
+            div.textContent = "";
+            div.style.border = "15px solid black";
+            div.style.backgroundColor = "black";
+            div.style.padding = "10px";
 
-    for(let r=0; r<16; r++){
-        // The Div Element Box
-        const div = document.createElement("div");
-        // Styling the Div Boxes
-        div.textContent = "Box";
-        div.style.border = "10px solid black";
-        div.style.backgroundColor = "gray";
-        div.style.margin = "8px";
-        // Add Div to Rows (16 each Row)
-        row.appendChild(div);
+            // Mouse Event Listeners 
+            div.addEventListener("mouseenter", () => {
+                if (mouseDown) {
+                    div.style.backgroundColor = "lightblue";
+                    div.style.border = "15px solid lightblue";
+                }
+            });
+
+             div.addEventListener("mousedown", () => {
+                div.style.backgroundColor = "lightblue";
+                div.style.border = "15px solid lightblue";
+            });
+
+            div.addEventListener("click", () => {
+                div.style.backgroundColor = "black";
+                div.style.border = "15px solid black";
+            });
+
+            // Add Div to Rows (16 each Row)
+            row.appendChild(div);
+        }
+        // Append The Row to Container
+        container.appendChild(row);
     }
-    // Append The Row to Container
-    container.appendChild(row);
 }
-
-
 
 
