9e47d17ff44b1f168573da09d73ce7078ad68fc3
bluemau531 <plazacabello531@gmail.com>
2025-07-26 16:07:11 -0500

    Finished sketch project

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-// Reference to Div in HTML Doc
 const container = document.getElementById("container");
+const myButton = document.getElementById("myButton");
+
 // Style for Container
 container.style.display = "inline-block";
 container.style.width = "auto";
 container.style.border = "5px solid white";
 
-
 // Logic for Holding Down Mouse
 let mouseDown = false;
-
 document.addEventListener("mousedown", () => mouseDown = true);
 document.addEventListener("mouseup", () => mouseDown = false);
 
 grid();
 
-function grid(){
-    // Grid Loop 16x16
-    for(let t=0; t<16; t++){
-    // Rows 1-16
-    const row = document.createElement("div");
-    row.style.display = "flex";
-        for(let r=0; r<16; r++){
-            // The Div Element Box
+function grid() {
+    for (let t = 0; t < 16; t++) {
+        const row = document.createElement("div");
+        row.style.display = "flex";
+
+        for (let r = 0; r < 16; r++) {
             const div = document.createElement("div");
-            // Styling the Div Boxes
             div.textContent = "";
             div.style.border = "15px solid black";
             div.style.backgroundColor = "black";
             div.style.padding = "10px";
 
-            // Mouse Event Listeners 
             div.addEventListener("mouseenter", () => {
                 if (mouseDown) {
                     div.style.backgroundColor = "lightblue";
@@ -37,7 +32,7 @@ function grid(){
                 }
             });
 
-             div.addEventListener("mousedown", () => {
+            div.addEventListener("mousedown", () => {
                 div.style.backgroundColor = "lightblue";
                 div.style.border = "15px solid lightblue";
             });
@@ -47,12 +42,17 @@ function grid(){
                 div.style.border = "15px solid black";
             });
 
-            // Add Div to Rows (16 each Row)
             row.appendChild(div);
         }
-        // Append The Row to Container
+
         container.appendChild(row);
     }
 }
 
-
+myButton.addEventListener("click", () => {
+    const boxes = container.querySelectorAll("div > div");
+    boxes.forEach(div => {
+        div.style.backgroundColor = "black";
+        div.style.border = "15px solid black";
+    });
+});
